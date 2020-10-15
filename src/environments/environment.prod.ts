453115089f9f7d8695c6468894a8d2e65614ce62
Ricardo <ric_bap@hotmail.com>
2020-10-15 18:22:29 -0300

    Deploy correção /

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://salvavidas-api.herokuapp.com/', /*'https://salvavidas-api.herokuapp.com',*/
+  apiUrl: 'https://salvavidas-api.herokuapp.com', /*'https://salvavidas-api.herokuapp.com',*/
 
   tokenWhitelistedDomains: [ /localhost:8080/ ],
   tokenBlacklistedRoutes: [ /\/oauth\/token/ ]
